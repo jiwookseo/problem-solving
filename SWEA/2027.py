bf89fe31b73edef7ab76d70b2bedb7958c8b76be
Jiwook Seo <spjo21@naver.com>
2019-02-11 13:49:30 +0900

    Add 20190211 algorithm practice

--- SWEA/2027.py
+++
@@ -1,4 +0,0 @@
-# 2027. 대각선 출력하기
-# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QFuZ6As0DFAUq&categoryId=AV5QFuZ6As0DFAUq&categoryType=CODE
-
-print("#++++\n+#+++\n++#++\n+++#+\n++++#")
--- SWEA/2047.py
+++
@@ -1,4 +0,0 @@
-# 2047. 신문 헤드라인 D1
-# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QKsLaAy0DFAUq&categoryId=AV5QKsLaAy0DFAUq&categoryType=CODE
-
-print(input().upper())
