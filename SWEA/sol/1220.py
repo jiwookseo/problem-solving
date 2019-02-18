9d0583456fda5ad234e1a362c848c8c3bba85429
Jiwook Seo <spjo21@naver.com>
2019-02-18 17:39:03 +0900

    Modify SWEA/sol/1220.py

--- SWEA/sol/1220.py
+++ SWEA/sol/1220.py
@@ -2,16 +2,15 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14hwZqABsCFAYD&categoryId=AV14hwZqABsCFAYD&categoryType=CODE
 for tc in range(1,11):
     n=int(input())
-    table=[input().replace(" ","").replace("0","") for _ in range(n)]
+    table=[input().split() for _ in range(n)]
     result=0
-    for col in range(len(table)):
-        check='0'
-        for row in range(len(col)):
+    for col in range(n):
+        check=0
+        for row in range(n):
             temp=table[row][col]
-            if temp=='1' and check!='1':
+            if temp=='1':
+                check=1
+            if temp=='2' and check==1:
+                check=2
                 result+=1
-            if temp!=check:
-                check=temp
-        if check=='1':
-            result-=1
     print(f"#{tc} {result}")
