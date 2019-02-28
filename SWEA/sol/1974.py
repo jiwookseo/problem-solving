6551ee27834b414f7e367169b698f85ac7d3ae14
Jiwook Seo <spjo21@naver.com>
2019-02-28 10:39:16 +0900

    Add 20190228 algorithm practice

---
+++ SWEA/sol/1974.py
@@ -0,0 +1,50 @@
+# 1974 스도쿠 검증 D2
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Psz16AYEDFAUq&categoryId=AV5Psz16AYEDFAUq&categoryType=CODE
+# ver.1
+# 내장 함수와 set 캐스팅을 적극적으로 사용함
+# ver.2에 비해 메모리 감소, 실행시간 증가
+# 메모리 49,100 kb
+# 실행시간 142 ms
+for tc in range(1,int(input())+1):
+    b=[]
+    result=1
+    for _ in range(9):
+        a=[False]*10
+        if result==1:
+            temp=list(map(int,input().split()))
+            t=[0]*10
+            for i in temp:
+                if a[i]:
+                    result=0
+                else:
+                    a[i]=True
+            b.append(temp)
+        else :
+            input()
+    c=0
+    while(result==1 and c<9):
+        a=[False]*10
+        for i in range(9):
+            t=b[i][c]
+            if a[t]:
+                result=0
+                break
+            else:
+                a[t]=True
+        c+=1
+    r=c=0
+    while(result==1 and r<3):
+        a=[False]*10
+        for dr in range(3):
+            for dc in range(3):
+                t=b[3*r+dr][3*c+dc]
+                if a[t]:
+                   result=0
+                   break
+                else:
+                    a[t]=True
+        c+=1
+        if c==3:
+            c=0
+            r+=1
+    print(f"#{tc} {result}")
---
+++ SWEA/sol/1974_ver3.py
@@ -0,0 +1,52 @@
+# 1974 스도쿠 검증 D2
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5Psz16AYEDFAUq&categoryId=AV5Psz16AYEDFAUq&categoryType=CODE
+# ver.3
+# board를 저장하지 않고 원소 별로 인덱스를 저장해 처리하는 방식
+# ver.1에 비교해서도 더욱 메모리를 적게 사용한다.
+# 메모리 48,052 kb
+# 실행시간 143 ms
+
+for tc in range(1,int(input())+1):
+    rb=[[] for _ in range(10)]
+    cb=[[] for _ in range(10)]
+    for r in range(9):
+        temp=list(map(int,input().split()))
+        t=[0]*10
+        tcb=[]
+        for c in range(9):
+            rb[temp[c]].append(r)
+            cb[temp[c]].append(c)
+    t=list(range(9))
+    tt=set(range(9))
+    result=1
+    for i in range(1,10):
+        if rb[i]!=t:
+            result=0
+            break
+        elif set(cb[i])!=tt:
+            result=0
+            break
+        for j in range(0,9,3):
+            check=[False]*3
+            for k in cb[i][j:j+3]:
+                if k<3:
+                    if check[0]:
+                        result=0
+                        break
+                    else:
+                        check[0]=True
+                elif k<6:
+                    if check[1]:
+                        result=0
+                        break
+                    else:
+                        check[1]=True
+                else:
+                    if check[2]:
+                        result=0
+                        break
+                    else:
+                        check[2]=True
+            if result==0:
+                break
+    print(f"#{tc} {result}")
