bcc0b2f4bec1d829fc89cab7f55e68c7425f312e
jiwookseo <jiwookseo.dev@gmail.com>
2019-03-20 00:03:11 +0900

    Add 20190319 SWEA Solving

--- SWEA/sol/5185.py
+++ SWEA/sol/5185.py
@@ -5,5 +5,5 @@ for tc in range(1, int(input()) + 1):
     ans = ''
     for i in range(n):
         for j in range(3, -1, -1):
-            ans += '1' if int(inp[1][i],16) & ( 1 << j ) else '0'
+            ans += '1' if int(inp[1][i], 16) & (1 << j) else '0'
     print("#{} {}".format(tc, ans))
--- SWEA/sol/5186.py
+++ SWEA/sol/5186.py
@@ -3,12 +3,12 @@ for tc in range(1, int(input()) + 1):
     num = float(input())
     result = ''
     for _ in range(12):
-        num*=2
+        num *= 2
         if num >= 1:
-            result+='1'
+            result += '1'
             num -= 1
         else:
-            result+='0'
-        if num==0:
+            result += '0'
+        if num == 0:
             break
-    print("#{} {}".format(tc,result if num==0 else "overflow"))
+    print("#{} {}".format(tc, result if num == 0 else "overflow"))
