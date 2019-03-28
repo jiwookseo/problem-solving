2f51277a101bb02d1b921d4e0d6496e741418bdd
Jiwook Seo <spjo21@naver.com>
2019-03-28 16:50:52 +0900

    Modify 20190328 SWEA Solving

--- SWEA/sol/1247.py
+++ SWEA/sol/1247.py
@@ -3,41 +3,41 @@
 # recursive 보다 stack 이 퍼포먼스가 좋다. Why? copy 보다 재귀 호출이 많은 것이 더 시간을 소요하는 것으로 보임.
 
 
+def dfs(i, count):
+    global result, visited, bp, lc
+    if result < count:
+        return
+    if visited == bp:
+        count += v[i][n + 1]
+        if result > count:
+            result = count
+        return
+    for j in range(1, n + 1):
+        if not visited[j]:
+            visited[j] = True
+            dfs(j, count + v[i][j])
+            visited[j] = False
+
+
 for TC in range(1, int(input()) + 1):
     n = int(input())
     lm = map(int, input().split())
-    lc = []
-    flag = 1
-    x = None
+    lc, flag, temp = [], 1, None
     for i in lm:
         if flag:
-            x = i
+            temp = i
         else:
-            lc.append((x, i))
+            lc.append((temp, i))
         flag = 1 - flag
-    start = lc.pop(0)
-    end = lc.pop(0)
-    stack = [(start, [False] * n, 0)]
-    bp = [True] * n
-    result = None
-    while stack:
-        (x, y), visited, count = stack.pop()
-        if result:
-            if result < count:
-                continue
-        if visited == bp:
-            nx, ny = end
-            count += abs(nx - x) + abs(ny - y)
-            if result:
-                if result > count:
-                    result = count
-            else:
-                result = count
-            continue
-        for i in range(n):
-            if not visited[i]:
-                nx, ny = lc[i]
-                temp = visited[:]
-                temp[i] = True
-                stack.append(((nx, ny), temp, count + abs(nx - x) + abs(ny - y)))
+    lc.append(lc.pop(1))
+    v = [[0] * (n + 2) for _ in range(n + 2)]
+    for i in range(n + 1):
+        for j in range(i, n + 2):
+            x1, y1 = lc[i]
+            x2, y2 = lc[j]
+            v[i][j] = v[j][i] = abs(x1 - x2) + abs(y1 - y2)
+    visited = [True] + [False] * n
+    bp = [True] * (n + 1)
+    result = 100000
+    dfs(0, 0)
     print("#{} {}".format(TC, result))
--- SWEA/sol/1865.py
+++ SWEA/sol/1865.py
@@ -2,6 +2,7 @@
 # https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LuHfqDz8DFAXc&categoryId=AV5LuHfqDz8DFAXc&categoryType=CODE
 # recursive 형태가 stack 형태보다 퍼포먼스가 40% 이상 좋다.
 # Because, stack 형태에 visited array 를 담기 위해서는 copy 를 해주어야 하기 때문에 시간 소모가 크다.
+# Greedy result 를 initial 값으로 두면, 그만큼 가지치기가 될 것이라 판단해 추가해보았으나, 퍼포먼스는 그리.. 메모리는 감소한다.
 
 
 def dfs(i, p):
@@ -21,7 +22,20 @@ def dfs(i, p):
 for TC in range(1, int(input()) + 1):
     n = int(input())
     pct = [list(map(lambda x: int(x) / 100, input().split())) for _ in range(n)]
-    visited = [False] * n
+    # # Initial result = Greedy result
+    # visited = [False] * n
+    # result = 1
+    # for i in range(n):
+    #     maxp = 0
+    #     ind = 0
+    #     for j in range(n):
+    #         if not visited[j]:
+    #             if maxp < pct[i][j]:
+    #                 maxp = pct[i][j]
+    #                 ind = j
+    #     visited[ind] = True
+    #     result *= maxp
     result = 0
+    visited = [False] * n
     dfs(0, 1)
     print("#{} {:.6f}".format(TC, round(result * 100, 6)))
